3b640f2a8907a186e647ce31f54c33cfb258aff5
Jason Tromborg <jtromborg1@gmail.com>
2023-04-17 17:46:54 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import copy from 'rollup-plugin-copy';
 export default defineConfig({
     plugins: [
         copy({
-            // copy over the calcite-components assets
             targets: [
                 {
                     src: 'node_modules/@esri/calcite-components/dist/calcite/assets/',
